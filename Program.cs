377f25c9e0638d2e4ccb694da6e2d341a7fd397f
“AMIR <amiraj1011@gmail.com>
2021-10-22 12:00:29 -0700

    [amir WELCOME TO GENERIC PROGRAM]

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics Program");
+        }
+    }
+}
